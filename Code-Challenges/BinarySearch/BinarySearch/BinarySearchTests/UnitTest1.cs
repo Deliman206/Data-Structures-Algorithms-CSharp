403c60ea87e9eef4bbebddbd4083808ae2af6e1c
Deliman206 <smiller206@me.com>
2019-01-14 10:29:20 -0800

    added tests for binarySearch

--- Code-Challenges/BinarySearch/BinarySearch/BinarySearchTests/UnitTest1.cs
+++ Code-Challenges/BinarySearch/BinarySearch/BinarySearchTests/UnitTest1.cs
@@ -1,14 +1,31 @@
 using System;
 using Xunit;
+using BinarySearch;
 
 namespace BinarySearchTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ValueDNE()
         {
+            int[] sortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; 
 
+            Assert.Equal(-1, BinarySearchIndex(sortedArray, 12));
+        }
+        [Fact]
+        public void ValueExistsEvenLengthArray()
+        {
+            int[] sortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Assert.Equal(3, BinarySearchIndex(sortedArray, 4));
+        }
+        [Fact]
+        public void ValueExistsOddLengthArray()
+        {
+            int[] sortedArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Assert.Equal(9, BinarySearchIndex(sortedArray, 9));
         }
     }
 }
