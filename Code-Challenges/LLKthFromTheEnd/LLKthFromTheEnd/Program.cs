a907d24d5144e114ae94aeb2359e878aff057299
Deliman206 <smiller206@me.com>
2019-01-16 11:02:02 -0800

    added tests for method

---
+++ Code-Challenges/LLKthFromTheEnd/LLKthFromTheEnd/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using LinkedList.Classes;
+
+namespace LLKthFromTheEnd
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Kth From the End Challenge!");
+        }
+        public static int KthFromTheEnd(LList lL, int k)
+        {
+            try
+            {
+                int linkListLength = 0;
+                lL.Current = lL.Head;
+
+                while (lL.Current != null)
+                {
+                    linkListLength += 1;
+                    lL.Current = lL.Current.Next;
+                }
+
+                lL.Current = lL.Head;
+                if (linkListLength - k - 1 < 0)
+                {
+                    throw new Exception();
+                }
+                for (int i = 0; i < linkListLength - k - 1; i++)
+                {
+                    lL.Current = lL.Current.Next;
+                }
+                return lL.Current.Value;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The linked list and K value do not work together. Use a K in the bounds of the linked list.");
+                throw;
+            }
+            
+        }
+    }
+}
